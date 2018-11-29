46f2752e55e5e6aedd1bf0e4cd2eab67a85ef9ab
demillan <demillan@live.com>
2018-11-29 23:49:03 +0100

    more improvements

--- zeld-dm.py
+++ zeld-dm.py
@@ -1,7 +1,20 @@
 import numpy as np
+import pylab as M
+
+# omega_m = 0.307115 
+# omega_l = 0.692885
+# omega_m=0.289796, omega_l=0.710204 # original
+def growthfunc(a, omega_m=0.307115, omega_l=0.692885):
+    da=a/10000.
+    integral = 0.
+    for i in range(10000):
+        aa=(i+1)*da
+        integral+=da/((aa*np.sqrt(omega_m/(aa**3)+omega_l))**3)
+    return 5*omega_m/2*np.sqrt(omega_m/a**3+omega_l)*integral
 
 # Parámetros iniciales
 
+redshift = 0
 boxsize = 128
 ngrid = 128
 
@@ -32,15 +45,29 @@ const1_L32 = 1/(boxsize**1.5)
 kmin = 2*np.pi/boxsize
 
 #setup particles on a uniform grid
-N = 256**3
-x1 = np.linspace(0, boxsize, num=N)
-x2 = np.linspace(0, boxsize, num=N)
-x3 = np.linspace(0, boxsize, num=N)
-x = np.vstack([x1,x2,x3])
+N = 32
+N3 = 32**3
+x1 = np.linspace(0, boxsize, N)
+x2 = np.linspace(0, boxsize, N)
+x3 = np.linspace(0, boxsize, N)
 
-sumx = np.zeros(N, np.complex)
+x1, x2, x3 = np.random.random((3, N))*boxsize
 
-for i in range(0, N):
+sk = (N, N, N)
+    
+x1 = np.fromfunction(lambda x,y,z:x, sk).astype(np.float)
+x1[np.where(x1 > ngrid/2)] -= ngrid
+x2 = np.fromfunction(lambda x,y,z:y, sk).astype(np.float)
+x2[np.where(x2 > ngrid/2)] -= ngrid
+x3 = np.fromfunction(lambda x,y,z:z, sk).astype(np.float)
+x3[np.where(x3 > ngrid/2)] -= ngrid
+
+M.scatter(x1, x2, s=1)
+M.show()
+
+dx = np.zeros(shape=(N3, 3), dtype = np.complex)
+
+for i in range(0, N3):
     for j in range(0, len(lmn_grid)):
         psum = 0
         if ((lmn_grid[j][0] != 0) & (lmn_grid[j][1] != 0) & (lmn_grid[j][2] != 0)):
@@ -48,5 +75,31 @@ for i in range(0, N):
             norm_delta_klmn = np.sqrt(-np.interp(np.sqrt(k2), pkinit[:,0], pkinit[:,1])*np.log(np.random.uniform(0, 1)))
             tetha_klmn = 2*np.pi*np.random.uniform(0, 1)
             delta_klmn = norm_delta_klmn * np.exp(tetha_klmn*1j)
-            psum = const1_L32 * 1j * (delta_klmn / k2) * np.exp(1j * kmin * np.dot([lmn_grid[j][0], lmn_grid[j][1], lmn_grid[j][2]], x[:,i]))
-        sumx[i] += psum
+            dsum = kmin * const1_L32 * 1j * (delta_klmn / k2) * np.exp(1j * kmin * np.dot([lmn_grid[j][0], lmn_grid[j][1], lmn_grid[j][2]], [x1.flatten()[i], x2.flatten()[i], x3.flatten()[i]]))*np.array([lmn_grid[j][0].astype(np.float), lmn_grid[j][1].astype(np.float), lmn_grid[j][2].astype(np.float)])
+        dx[i] += dsum
+
+fx = dx[:,0]
+fy = dx[:,1]
+fz = dx[:,2]
+
+#displacements, scaled by the growth function at the redshift we want
+d1=growthfunc(1./(1+redshift))/growthfunc(1.)
+xdisp=fx.real*d1
+ydisp=fy.real*d1
+zdisp=fz.real*d1
+    
+#assuming ngrid=nparticles, displace particles from the grid
+x1+=xdisp
+x2+=ydisp
+x3+=zdisp
+    
+#periodic boundary conditions
+x1[np.where(x1<0)]+=boxsize
+x1[np.where(x1>boxsize)]-=boxsize
+x2[np.where(x2<0)]+=boxsize
+x2[np.where(x2>boxsize)]-=boxsize
+x3[np.where(x3<0)]+=boxsize
+x3[np.where(x3>boxsize)]-=boxsize
+
+M.scatter(x1, x2, s=0.5)
+M.show()
